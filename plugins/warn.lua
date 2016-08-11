fc79e5ae38405fdda1fa40a3eafac155ccfaa4f6
s20team <salehfffaaa@gmail.com>
2016-08-11 17:19:32 +0430

    Update warn.lua

--- plugins/warn.lua
+++ plugins/warn.lua
@@ -4,20 +4,19 @@
 #             Warn             #
 #                              #
 #                              #
+#    by king_h4ck_iran         #
+#    fixed by @stf_tem         #
+#                              #
+#    Modified by stf_tem       #
+#	                       #
+#                              #
+#     Update: 3 August 2016    #
 #                              #
 #                              #
-#                              #
-#                              #
-#	                            #
-#                              #
-#                              #
-#                              #
-#                              #
-#           @OMEGA_SHIELD      #
-#                              #
+#           @GPMod             #
+#         @JustLua             #
 ################################
 ]]
-
 local function warn_by_username(extra, success, result) -- /warn <@username>
   if success == 1 then  
   local msg = result
@@ -34,31 +33,31 @@ local function warn_by_username(extra, success, result) -- /warn <@username>
   end
 ----------------------------------
   if is_momod2(msg.id, target) and not is_admin2(extra.fromid) then
-  return send_msg(receiver, 'You can not warn to mods', ok_cb, false) end
+  return send_msg(receiver, 'شما نمیتوانید به مدیر گروه اخطار بدهید!', ok_cb, false) end
 --endif--
-  if is_admin2(msg.id) then return send_msg(receiver, 'You can not warn to bot admin', ok_cb, false) end
+  if is_admin2(msg.id) then return send_msg(receiver, 'شما نمیتوانید به ادمین ربات اخطار بدهید!', ok_cb, false) end
 --endif--
   if value then
    if value == '1' then
     redis:hset(hash, msg.id, '2')
-   text = '[ '..name..' ]\n You receive a warning for failure to comply with laws\nThe number of warnings you: 2/4'
+   text = '[ '..name..' ]\n شما به دلیل رعایت نکردن قوانین اخطار دریافت میکنید\nتعداد اخطار های شما : ۲/۴'
    elseif value == '2' then
   redis:hset(hash, msg.id, '3')
-  text = '[ '..name..' ]\n You receive a warning for failure to comply with laws\nThe number of warnings you: 3/4'
+  text = '[ '..name..' ]\n شما به دلیل رعایت نکردن قوانین اخطار دریافت میکنید\nتعداد اخطار های شما : ۳/۴'
    elseif value == '3' then
    redis:hdel(hash, msg.id, '0')
    local hash =  'banned:'..target
    redis:sadd(hash, msg.id)
-  text = '[ '..name..' ]\n was fired because of failure to comply with laws\nThe number of incoming Warning: 4/4'
+  text = '[ '..name..' ]\n به دلیل رعایت نکردن قوانین از گروه اخراج شد (banned)\nتعداد اخطار ها : ۴/۴'
   chat_del_user(receiver, 'user#id'..msg.id, ok_cb, false)
    end
   else
    redis:hset(hash, msg.id, '1')
-   text = '[ '..name..' ]\n You receive a warning for failure to comply with laws\nThe number of warnings you: 1/4'
+   text = '[ '..name..' ]\n شما به دلیل رعایت نکردن قوانین اخطار دریافت میکنید\nتعداد اخطار های شما : ۱/۴'
   end
   send_msg(receiver, text, ok_cb, false)
   else
-   send_msg(receiver, ' Username not found', ok_cb, false)
+   send_msg(receiver, ' نام کاربری پیدا نشد.', ok_cb, false)
   end
 end
 
@@ -79,27 +78,27 @@ local function warn_by_reply(extra, success, result) -- (on reply) /warn
   end
 ----------------------------------
   if is_momod2(msg.from.id, msg.to.id) and not is_admin2(extra.fromid) then
-  return send_msg(receiver, 'You can not warn to mods', ok_cb, false) end
+  return send_msg(receiver, 'شما نمیتوانید به مدیر گروه اخطار بدهید!', ok_cb, false) end
 --endif--
-  if is_admin2(msg.from.id) then return send_msg(receiver, 'You can not warn to bot admin', ok_cb, false) end
+  if is_admin2(msg.from.id) then return send_msg(receiver, 'شما نمیتوانید به ادمین ربات اخطار بدهید!', ok_cb, false) end
 --endif--
   if value then
    if value == '1' then
     redis:hset(hash, msg.from.id, '2')
-   text = '[ '..name..' ]\n You receive a warning for failure to comply with laws\nThe number of warnings you: 2/4'
+   text = '[ '..name..' ]\n .شما به دلیل رعایت نکردن قوانین اخطار دریافت میکنید\nتعداد اخطار های شما : ۲/۴'
    elseif value == '2' then
   redis:hset(hash, msg.from.id, '3')
-  text = '[ '..name..' ]\n You receive a warning for failure to comply with laws\nThe number of warnings you: 3/4'
+  text = '[ '..name..' ]\n شما به دلیل رعایت نکردن قوانین اخطار دریافت میکنید.\nتعداد اخطار های شما : ۳/۴'
    elseif value == '3' then
    redis:hdel(hash, msg.from.id, '0')
-  text = '[ '..name..' ]\n was fired because of failure to comply with laws\nThe number of incoming Warning: 4/4'
+  text = result.to.peer_id..'[ '..name..' ]\n به دلیل رعایت نکردن قوانین از گروه اخراج شد. (banned)\nتعداد اخطار ها : ۴/۴'
   local hash =  'banned:'..target
   redis:sadd(hash, msg.from.id)
-  chat_del_user(receiver, 'user#id'..msg.from.id, ok_cb, false)
+  kick_user_any(msg.from.peer_id, result.to.peer_id)
    end
   else
    redis:hset(hash, msg.from.id, '1')
-   text = '[ '..name..' ]\n You receive a warning for failure to comply with laws\nThe number of warnings you: 1/4'
+   text = '[ '..name..' ]\n شما به دلیل رعایت نکردن قوانین اخطار دریافت میکنید.\nتعداد اخطار های شما : ۱/۴'
   end
   reply_msg(extra.Reply, text, ok_cb, false)
 end
@@ -121,13 +120,13 @@ local function unwarn_by_username(extra, success, result) -- /unwarn <@username>
 --endif--
   if value then
   redis:hdel(hash, msg.id, '0')
-  text = 'User warning ('..msg.id..') Deleted.\nThe number of warnings: 0/4'
+  text = 'اخطار های کاربر ('..msg.id..') پاک شد\nتعداد اخطار ها : ۰/۴'
   else
-   text = 'This advance notice is not received'
+   text = 'این کاربر اخطاری دریافت نکرده است'
   end
   send_msg(receiver, text, ok_cb, false)
   else
-   send_msg(receiver, ' Username not found', ok_cb, false)
+   send_msg(receiver, ' نام کاربری پیدا نشد.', ok_cb, false)
   end
 end
 
@@ -148,9 +147,9 @@ local function unwarn_by_reply(extra, success, result) -- (on reply) /unwarn
 --endif--
   if value then
   redis:hdel(hash, msg.from.id, '0')
-  text = 'User warning ('..msg.from.id..') Deleted.\nThe number of warnings: 0/4'
+  text = 'اخطار های کاربر ('..msg.from.id..') پاک شد\nتعداد اخطار ها : ۰/۴'
   else
-   text = 'This advance notice is not received'
+   text = 'این کاربر اخطاری دریافت نکرده است'
   end
   reply_msg(extra.Reply, text, ok_cb, false)
 end
@@ -165,19 +164,19 @@ local function run(msg, matches)
  local receiver = get_receiver(msg)
  if msg.to.type == 'user' then return end
  --endif--
- if not is_momod(msg) then return 'You are not a manager' end
+ if not is_momod(msg) then return 'شما مدیر نیستید' end
  --endif--
  ----------------------------------
  if matches[1]:lower() == 'warn' and not matches[2] then -- (on reply) /warn
   if msg.reply_id then
     local Reply = msg.reply_id
     msgr = get_message(msg.reply_id, warn_by_reply, {receiver=receiver, Reply=Reply, target=target, fromid=fromid})
-  else return 'The username or reply messages to warn users use' end
+  else return 'از نام کاربری یا ریپلی کردن پیام کاربر برای اخطار دادن استفاده کنید' end
  --endif--
  end
  if matches[1]:lower() == 'warn' and matches[2] then -- /warn <@username>
    if string.match(user, '^%d+$') then
-      return 'The username or reply messages to warn users use'
+      return 'از نام کاربری یا ریپلی کردن پیام کاربر برای اخطار دادن استفاده کنید'
     elseif string.match(user, '^@.+$') then
       username = string.gsub(user, '@', '')
       msgr = res_user(username, warn_by_username, {receiver=receiver, user=user, target=target, fromid=fromid})
@@ -187,12 +186,12 @@ local function run(msg, matches)
   if msg.reply_id then
     local Reply = msg.id
     msgr = get_message(msg.reply_id, unwarn_by_reply, {receiver=receiver, Reply=Reply, target=target, fromid=fromid})
-  else return 'The username or reply messages to unwarn users use' end
+  else return 'از نام کاربری یا ریپلی کردن استفاده کنید' end
  --endif--
  end
  if matches[1]:lower() == 'unwarn' and matches[2] then -- /unwarn <@username>
    if string.match(user, '^%d+$') then
-      return 'The username or reply messages to unwarn users use'
+      return 'از نام کاربری یا ریپلی کردن استفاده کنید'
     elseif string.match(user, '^@.+$') then
       username = string.gsub(user, '@', '')
       msgr = res_user(username, unwarn_by_username, {receiver=receiver, user=user, target=target, fromid=fromid})
@@ -209,5 +208,3 @@ return {
   }, 
   run = run 
 }
-
---By Arian
